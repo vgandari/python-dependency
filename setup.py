592263119f7fca71daa478838319af521e648ce7
vgandari <victor.gandarillas@icloud.com>
2021-05-25 08:12:00 -0700

    remove setuptools-git-versioning dependency

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(
     name='python-dependency',
-    # this will read the version number from the current commit's tag
-    setup_requires=['setuptools-git-versioning'],
     packages=[
         'python-dependency',
     ],
